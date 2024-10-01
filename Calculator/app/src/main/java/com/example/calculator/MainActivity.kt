fef1fd7fcdadc72a5beb5aa581efa0f2af9c2454
Margarida Amaral <a29849@alunos.ipca.pt>
2024-10-01 10:50:09 +0100

    inital commit

---
+++ Calculator/app/src/main/java/com/example/calculator/MainActivity.kt
@@ -0,0 +1,56 @@
+package com.example.calculator
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.calculator.ui.theme.CalculatorTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            CalculatorTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    CalculatorScreen(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun CalculatorScreen(modifier: Modifier = Modifier) {
+    Column(modifier = modifier) {
+        Text(text = "0")
+        Row {
+            Button(onClick = { /*TODO*/ }) {
+                Text(text = "7")
+            }
+            Button(onClick = { /*TODO*/ }) {
+                Text(text = "8")
+            }
+            Button(onClick = { /*TODO*/ }) {
+                Text(text = "9")
+            }
+           
+
+            }
+        }
+    }
+}
+
+
